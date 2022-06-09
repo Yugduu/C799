a1520a0225ff8cfc83911250487e7af83a0dd88b
Yugduu <103189236+Yugduu@users.noreply.github.com>
2022-06-09 13:36:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function submit()
+
+var display_student_array []
+
+for  (var j = 1  j <=50   j++)
+
+var name_of_the_student = document.getElementById("name_of_the_student"+j).value;
+console.log(name_of_the_student);
+
+var lenght_of_the_name_of_
